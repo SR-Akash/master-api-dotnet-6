fd8383306255dc96f13d845562fff7b69f082221
sr-akash <akashtah25@gmail.com>
2024-03-07 00:44:02 +0600

    periodic timer setup

--- master-api-dotnet 6/Repository/BackgroundService/BackgroupServiceRepo.cs
+++ master-api-dotnet 6/Repository/BackgroundService/BackgroupServiceRepo.cs
@@ -11,35 +11,77 @@ namespace master_api_dotnet_6.Repository
         }
 
 
+        #region this is one way
+        //protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        //{
+        //    while (!stoppingToken.IsCancellationRequested)
+        //    {
+        //        using (var scope = _serviceProvider.CreateScope())
+        //        {
+        //            var _context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
+        //            var user = new TblUser
+        //            {
+        //                ConfirmPassword = "Akash25@",
+        //                Email = "akash@ibos.io",
+        //                IsActive = true,
+        //                IsDelete = false,
+        //                LastActionDatetime = DateTime.UtcNow,
+        //                Mobile = "01634860323",
+        //                Password = "Akash25@",
+        //                PreviousPassword = "Akash25@",
+        //                UserName = "Akash"
+        //            };
+
+        //            _context.TblUsers.Add(user);
+        //            await _context.SaveChangesAsync();
+        //        };
+        //        await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken); // Delay for 30 seconds
+
+
+        //    }
+        //}
+        #endregion
+
+        #region this is another way background service
+
+        private readonly PeriodicTimer _timer = new(TimeSpan.FromSeconds(5));
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            while (await _timer.WaitForNextTickAsync(stoppingToken)
+                && !stoppingToken.IsCancellationRequested)
             {
-                using (var scope = _serviceProvider.CreateScope())
-                {
-                    var _context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-
-                    var user = new TblUser
-                    {
-                        ConfirmPassword = "Akash25@",
-                        Email = "akash@ibos.io",
-                        IsActive = true,
-                        IsDelete = false,
-                        LastActionDatetime = DateTime.UtcNow,
-                        Mobile = "01634860323",
-                        Password = "Akash25@",
-                        PreviousPassword = "Akash25@",
-                        UserName = "Akash"
-                    };
-
-                    _context.TblUsers.Add(user);
-                    await _context.SaveChangesAsync();
-                };
-                await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken); // Delay for 30 seconds
-
-
+                DoWorkAsync();
             }
         }
+
+
+        private async void DoWorkAsync()
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var _context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+                var user = new TblUser
+                {
+                    ConfirmPassword = "Akash25@",
+                    Email = "akash@ibos.io",
+                    IsActive = true,
+                    IsDelete = false,
+                    LastActionDatetime = DateTime.UtcNow,
+                    Mobile = "01634860323",
+                    Password = "Akash25@",
+                    PreviousPassword = "Akash25@",
+                    UserName = "Akash"
+                };
+
+                _context.TblUsers.Add(user);
+                await _context.SaveChangesAsync();
+            };
+        }
+
+
+
+        #endregion
     }
 }
